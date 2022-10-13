a7b1eb70179aee551b60adfaca3e1f018b7c4af6
ArtBulSpb <arturtomsk2005@gmail.com>
2022-10-13 17:04:15 +0300

    fix bugs in class3.py

--- class3.py
+++ class3.py
@@ -1,40 +1,50 @@
+from datetime import date
+
+now = date.today()
+
+
 class human:
-    def namechange(self): #метод создания имени
+    def namechange(self):  # метод создания имени
         name = input("введите имя ")
         if name == "":
             name = "artur"
             print("оставлено стандартное имя ", name)
         else:
-            print("Имя изменено на " + name)
-    def yearchange(self):#метод определения возраста
+            print("Имя изменено на " + name, "\n", "Здравствуйте, ", name, "!\n")
+        return name
+
+    def yearchange(self):  # метод определения возраста
+
         while True:
+            year = int(now.year) + 1
+            print(year)
+
+            while year > now.year:
+                try:
+                    print("try")
+                    year = int(input(("введите год рождения. Он должеть быть меньше, чем " + str(now.year) + " ")))
+
+                except:
+                    year = 2005
+                    print("год рождения введен неверно, подставлено стандертное значение ", year)
+
             try:
-                year = int(input("введите год рождения "))
-            except:
-                year = 2005
-                print("год рождения введен неверно, подставлено стандертное значение ", year)
-            try:
-                newyear = int(input("введите год, возраст человека в котором хотите узнать "))
+                newyear = int(input("введите год, в котором хотите узнать возраст человека  "))
             except:
                 newyear = 2022
 
-            old = newyear - year
-            print("возраст = ", old)
+            age = newyear - year
+            print("возраст = ", age)
             break
+
+
 class student(human):
-    name = human.namechange()#наследование имени
-    old = human.yearchange()#наследование возраста
+    def study(self, old):
+        for old in range(17, 22):
+            kyrc = kyrc + 1
+            print("курс ", kyrc)
 
-    def name2(self):
-        print(name)
+
 info = human()
-info.namechange()#использование методов
-info.yearchange()#использование методов
-info2 = student
-info2.name2()#использование методов
-
-
-
-
-
-
+info.namechange()  # использование методов
+info.yearchange()  # использование методов
