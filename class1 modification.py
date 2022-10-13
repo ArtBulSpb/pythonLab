8359d7eb0890e5a654e94638f20cbca066440f6f
ArtBulSpb <arturtomsk2005@gmail.com>
2022-10-13 14:17:25 +0300

    tuning for class1 modification.py done

--- class1 modification.py
+++ class1 modification.py
@@ -1,12 +1,13 @@
 import random
 
+
 class quadrate:
 
     def perimeter(self, a):
-        print("периметр = ", a*4, "\n")
+        print("периметр = ", a * 4, "\n")
 
     def square(self, a):
-        print("площадь = ", a*a, "\n")
+        print("площадь = ", a * a, "\n")
 
     def request(self):
         try:
@@ -16,9 +17,9 @@ class quadrate:
             print("число введено неверно, использовано рандомное значение ", i)
         return i
 
+
 quad1 = quadrate()
 quad2 = quadrate()
-
 
 print("Вызываем метод perimeter")
 quad1.perimeter(quad1.request())  # Hello work
@@ -26,5 +27,3 @@ quad1.perimeter(quad1.request())  # Hello work
 print("Вызываем методы perimeter и square")
 quad2.square(quad1.request())
 quad2.perimeter(quad1.request())
-
-
