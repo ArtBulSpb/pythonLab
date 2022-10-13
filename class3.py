04657984e76eaf6068cead508d4df890fc23cc6e
ArtBulSpb <arturtomsk2005@gmail.com>
2022-10-13 20:23:20 +0300

    fix all, stable

--- class3.py
+++ class3.py
@@ -1,50 +1,68 @@
 from datetime import date
+import random
 
 now = date.today()
+study_time = 5
 
 
 class human:
+
     def namechange(self):  # метод создания имени
-        name = input("введите имя ")
+        name = input("Введите имя ")
         if name == "":
             name = "artur"
             print("оставлено стандартное имя ", name)
         else:
-            print("Имя изменено на " + name, "\n", "Здравствуйте, ", name, "!\n")
+            print("Здравствуйте, ", name, "!\n")
         return name
 
     def yearchange(self):  # метод определения возраста
 
-        while True:
-            year = int(now.year) + 1
-            print(year)
+        year = int(now.year) + 1
 
-            while year > now.year:
-                try:
-                    print("try")
-                    year = int(input(("введите год рождения. Он должеть быть меньше, чем " + str(now.year) + " ")))
-
-                except:
-                    year = 2005
-                    print("год рождения введен неверно, подставлено стандертное значение ", year)
-
+        while year > now.year:
             try:
-                newyear = int(input("введите год, в котором хотите узнать возраст человека  "))
-            except:
-                newyear = 2022
+                year = int(input(("Введите год рождения. Он должеть быть меньше, чем " + str(now.year) + " ")))
 
-            age = newyear - year
-            print("возраст = ", age)
-            break
+            except:
+                year = 2005
+                print("год рождения введен неверно, подставлено стандертное значение ", year)
+
+        try:
+            newyear = int(input("Введите год, в котором хотите узнать возраст человека  "))
+        except:
+            newyear = 2022
+
+        age = newyear - year
+        print("Возраст в ", newyear, "году будет ", age)
+        return age
 
 
 class student(human):
-    def study(self, old):
-        for old in range(17, 22):
-            kyrc = kyrc + 1
-            print("курс ", kyrc)
+    def __init__(self, age):
+        self.age = age
+        # print("init, age = ", age)
+
+    def study(self):
+
+        if self.age >= 17:  # and self.age <= 22:
+            start_course = 0
+
+        else:
+
+            if self.age < 17:
+                start_course = 17 - self.age
+
+        return start_course
 
 
-info = human()
-info.namechange()  # использование методов
-info.yearchange()  # использование методов
+info = human()  # Создаём экземпляр класса
+name = info.namechange()  # Вызываем метод класса human()
+
+info2 = student(info.yearchange())
+info2.study()
+print("Поздравляем, ", name)
+print("Вы зачислены в группу ", random.randint(100, 200))
+print("Начало курса: ", now.year + info2.study())
+print("Окончание курса: ", now.year + info2.study() + study_time)
+
