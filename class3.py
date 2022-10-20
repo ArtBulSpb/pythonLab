4c8dfe473281a8c5b9dd23c74e5ec5b8f984f3a9
ArtBulSpb <arturtomsk2005@gmail.com>
2022-10-20 22:55:52 +0300

    Промежуточный результат ОК. Нужно избавиться от двойного исполнния yearchange

--- class3.py
+++ class3.py
@@ -27,45 +27,59 @@ class human:
             except:
                 year = 2005
                 print("год рождения введен неверно, подставлено стандертное значение ", year)
+            newyear = " "
+            while (newyear == " "):
+                try:
+                    newyear = int(input("Введите год, в котором хотите узнать возраст человека  "))
+                except:
+                    print("год введен неверно ")
 
-        try:
-            # TODO Обработать ввод года меньше года рождения
-            newyear = int(input("Введите год, в котором хотите узнать возраст человека  "))
-        except:
-            newyear = 2022
+            while (newyear < year):
+                print("в этом году человек еще не родился ")
+                try:
+                    newyear = int(input("Введите год, в котором хотите узнать возраст человека  "))
+                except:
+                    print("год введен неверно ")
+        print("43 NY = ", newyear)
+        return int(year), int(newyear)
 
+    def agecount(self, newyear, year):
         age = newyear - year
         print("Возраст в ", newyear, "году будет ", age)
         return age
 
-
 class student(human):
-    def __init__(self, age):
+    def __init__(self, age, newyear):
         self.age = age
+        self.newyear = newyear
         # print("init, age = ", age)
 
     def study(self):
 
-        if self.age >= 17:  # and self.age <= 22:
+        if self.age >= 17 and self.age <= 27:  # and self.age <= 22:
             start_course = 0
 
         else:
 
             if self.age < 17:
+                print(self.age)
                 start_course = 17 - self.age
+                print(start_course)
 
         return start_course
     def course(self, start_course):
         for i in range (start_course, start_course + study_time - 2):
-            print("год обучения", i + 1)
+            print("год обучения", i + 1 + self.newyear)
 
 
 
 info = human()  # Создаём экземпляр класса
 name = info.namechange()  # Вызываем метод класса human()
 
-info2 = student(info.yearchange())
+info2 = student(info.yearchange()[0], info.yearchange()[1])
+
 info2.study()
+
 print("Поздравляем, ", name)
 print("Вы зачислены в группу ", random.randint(100, 200))# Назначение группы обучения
 print("Начало курса: ", now.year + info2.study()) # Начало занятий
--- class5.py
+++ class5.py
@@ -20,25 +20,32 @@ class exchange:
 
 
             try:
-                a = int(input("введите курс "))
+                a = int(input("введите курс в рублях "))
             except:
                 print("курс введен неправильно ")
                 break
-            exchange_rate.append(a)
-            if datecounter == 1:
-                exchange_rate_min = a
-                print("25 a =  ", a)
-                print("26 exchange_rate_min=  ", exchange_rate_min)
-                print("27 counter=  ", counter)
-            else:
-                if a < exchange_rate_min:
-                    print("a =  ", a)
+            if a > 0:
+                exchange_rate.append(a)
+                if datecounter == 1:
                     exchange_rate_min = a
-                    print("32 exchange_rate_min=  ", exchange_rate_min)
-                    counter = datecounter
-                    print("34 counter=  ", counter)
-            print("день ", datecounter, exchange_rate)
-        print("минимальный курс ", value, "= ", exchange_rate_min, " в ", counter, " день")
+                    # print("25 a =  ", a)
+                    # print("26 exchange_rate_min=  ", exchange_rate_min)
+                    # print("27 counter=  ", counter)
+                else:
+                    if a < exchange_rate_min:
+                        print("a =  ", a)
+                        exchange_rate_min = a
+                        # print("32 exchange_rate_min=  ", exchange_rate_min)
+                        counter = datecounter
+                        # print("34 counter=  ", counter)
+                print("день ", datecounter, exchange_rate)
+            else:
+                print("курс не может быть отрицательным ")
+                break
+        if counter != 0:
+            print("минимальный курс ", value, "= ", exchange_rate_min, " в ", counter, " день")
+        else:
+            print("минимальный курс ", value, "= ", exchange_rate_min, "руб   в ", counter + 1, " день")
 
 
 
